c6bfc48d5d562cdf4c083fb852d374eafdd25cf6
potakaaa <helbirog@gmail.com>
2024-11-23 19:17:04 +0800

    Added navigation

--- src/pages/AddNotes.tsx
+++ src/pages/AddNotes.tsx
@@ -4,18 +4,29 @@ import "./AddNotes.css";
 
 const AddNotes = () => {
   const [title, setTitle] = useState("");
+  const [notesBody, setNotesBody] = useState("");
 
   return (
     <div className="container">
-      <input
-        className="note-title"
-        type="text"
-        value={title}
-        placeholder="Type note title..."
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <textarea className="note-body" placeholder="Type note..." />
-      <button className="btn btn-primary">Save</button>
+      <div className="note-title-container">
+        <input
+          className="note-title"
+          type="text"
+          value={title}
+          placeholder="Type note title..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="note-body-container">
+        <textarea className="note-body" placeholder="Type note..." />
+      </div>
+
+      <div className="note-btn-container">
+        <button id="note-btn" className="btn btn-primary">
+          Save
+        </button>
+      </div>
     </div>
   );
 };
